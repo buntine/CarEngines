61523ff246823f98d4e257154e6c550502a69d6c
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-24 22:08:45 +1000

    Increased frequency jumps

--- engine.js
+++ engine.js
@@ -1,29 +1,10 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
     osc = audioCtx.createOscillator(),
     gain = audioCtx.createGain(),
-    distortion = audioCtx.createWaveShaper(),
     freqDiff = 0,
     minFreq = 69,
     maxFreq = 121;
 
-function makeDistortionCurve(amount) {
-  var k = typeof amount === 'number' ? amount : 50,
-      n_samples = 44100,
-      curve = new Float32Array(n_samples),
-      deg = Math.PI / 180,
-      i = 0,
-      x;
-
-  for ( ; i < n_samples; ++i ) {
-    x = i * 2 / n_samples - 1;
-    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
-  }
-
-  return curve;
-};
-
-distortion.curve = makeDistortionCurve(400);
-osc.connect(distortion);
 osc.connect(gain);
 gain.connect(audioCtx.destination);
 
@@ -33,11 +14,11 @@ gain.gain.value = 0.2;
 osc.start();
 
 window.onkeydown = function(e) {
-  freqDiff = 1;
+  freqDiff = 2;
 };
 
 window.onkeyup = function(e) {
-  freqDiff = -1;
+  freqDiff = -2;
 };
 
 setInterval(function(){
