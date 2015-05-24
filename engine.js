26bbecda9acfb3482dda12fbec2e500c55b61cd4
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-24 21:35:11 +1000

    Playing around with waveshaper

--- engine.js
+++ engine.js
@@ -1,11 +1,30 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
     osc = audioCtx.createOscillator(),
-    gain = audioCtx.createGain();
+    gain = audioCtx.createGain(),
+    distortion = audioCtx.createWaveShaper();
 
+function makeDistortionCurve(amount) {
+  var k = typeof amount === 'number' ? amount : 50,
+      n_samples = 44100,
+      curve = new Float32Array(n_samples),
+      deg = Math.PI / 180,
+      i = 0,
+      x;
+
+  for ( ; i < n_samples; ++i ) {
+    x = i * 2 / n_samples - 1;
+    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+  }
+
+  return curve;
+};
+
+distortion.curve = makeDistortionCurve(400);
+osc.connect(distortion);
 osc.connect(gain);
 gain.connect(audioCtx.destination);
 
-osc.type = "sine";
+osc.type = "sawtooth";
 osc.frequency.value = 50;
-gain.gain.value = 1.0;
+gain.gain.value = 0.3;
 osc.start();
