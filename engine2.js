d0777641d60c0989455e0fc946769b2a9045454e
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-25 23:18:15 +1000

    Setup looping

--- engine2.js
+++ engine2.js
@@ -2,13 +2,14 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
     source = audioCtx.createBufferSource(),
     xhr = new XMLHttpRequest();
 
-xhr.open("GET", "/sounds/engine.mp3", true);
+xhr.open("GET", "/sounds/engine.wav", true);
 xhr.responseType = "arraybuffer";
 xhr.onload = function(e){
   audioCtx.decodeAudioData(this.response, function(buffer) {
     source.buffer = buffer;
+    source.loop = true;
     source.connect(audioCtx.destination);
-    source.start(0);
+    source.start();
   });
 };
 
