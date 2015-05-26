64c193c00e724c43b485b581c52fb80433e5eef2
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-26 21:51:44 +1000

    Adding start/stop to engine1

--- engine1.js
+++ engine1.js
@@ -1,34 +1,52 @@
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
-    osc = audioCtx.createOscillator(),
-    gain = audioCtx.createGain(),
+window.onload = function(){
+var start = document.getElementById("start"),
+    stop = document.getElementById("stop"),
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
     freqDiff = 0,
     minFreq = 69,
-    maxFreq = 121;
+    maxFreq = 121,
+    osc,
+    intervalId,
+    incFreqDiff,
+    decFreqDiff;
 
-osc.connect(gain);
-gain.connect(audioCtx.destination);
-
-osc.type = "sawtooth";
-osc.frequency.value = 70;
-gain.gain.value = 0.2;
-osc.start();
-
-window.onkeydown = function(e) {
-  if (e.keyCode == 38) {
-    freqDiff = 1;
+function createFreqDiff(n) {
+  return function(e) {
+    if (e.keyCode == 38) {
+      freqDiff = n;
+    }
   }
+}
+
+incFreqDiff = createFreqDiff(1);
+decFreqDiff = createFreqDiff(-1);
+
+start.addEventListener("click", function(e) {
+  var gain = audioCtx.createGain();
+   
+  osc = audioCtx.createOscillator(),
+  osc.connect(gain);
+  gain.connect(audioCtx.destination);
+  osc.type = "sawtooth";
+  osc.frequency.value = 70;
+  gain.gain.value = 0.2;
+  osc.start();
+
+  window.addEventListener("keydown", incFreqDiff);
+  window.addEventListener("keyup", decFreqDiff);
+
+  intervalId = setInterval(function(){
+    var currFreq = osc.frequency.value;
+
+    if ((freqDiff < 0 && currFreq > minFreq) || (freqDiff > 0 && currFreq < maxFreq)) {
+      osc.frequency.value += freqDiff;
+    }
+  }, 40);
+});
+
+stop.addEventListener("click", function(e) {
+  window.removeEventListener("keydown", incFreqDiff);
+  window.removeEventListener("keyup", decFreqDiff);
+  clearInterval(intervalId);
+});
 };
-
-window.onkeyup = function(e) {
-  if (e.keyCode == 38) {
-    freqDiff = -1;
-  }
-};
-
-setInterval(function(){
-  var currFreq = osc.frequency.value;
-
-  if ((freqDiff < 0 && currFreq > minFreq) || (freqDiff > 0 && currFreq < maxFreq)) {
-    osc.frequency.value += freqDiff;
-  }
-}, 40);
