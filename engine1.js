0d3b1c4486640ac6feb40e9417239fb909d8d45e
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-26 21:55:08 +1000

    Small refactor of engine1

--- engine1.js
+++ engine1.js
@@ -1,52 +1,51 @@
 window.onload = function(){
-var start = document.getElementById("start"),
-    stop = document.getElementById("stop"),
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
-    freqDiff = 0,
-    minFreq = 69,
-    maxFreq = 121,
-    osc,
-    intervalId,
-    incFreqDiff,
-    decFreqDiff;
+  var start = document.getElementById("start"),
+      stop = document.getElementById("stop"),
+      audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
+      freqDiff = 0,
+      minFreq = 69,
+      maxFreq = 121,
+      osc, intervalId, incFreqDiff, decFreqDiff;
 
-function createFreqDiff(n) {
-  return function(e) {
-    if (e.keyCode == 38) {
-      freqDiff = n;
+  function createFreqDiff(n) {
+    return function(e) {
+      if (e.keyCode == 38) {
+        freqDiff = n;
+      }
     }
   }
-}
 
-incFreqDiff = createFreqDiff(1);
-decFreqDiff = createFreqDiff(-1);
+  incFreqDiff = createFreqDiff(1);
+  decFreqDiff = createFreqDiff(-1);
 
-start.addEventListener("click", function(e) {
-  var gain = audioCtx.createGain();
-   
-  osc = audioCtx.createOscillator(),
-  osc.connect(gain);
-  gain.connect(audioCtx.destination);
-  osc.type = "sawtooth";
-  osc.frequency.value = 70;
-  gain.gain.value = 0.2;
-  osc.start();
+  start.addEventListener("click", function(e) {
+    var gain = audioCtx.createGain();
+     
+    osc = audioCtx.createOscillator(),
+    osc.connect(gain);
+    gain.connect(audioCtx.destination);
+    osc.type = "sawtooth";
+    osc.frequency.value = 70;
+    gain.gain.value = 0.2;
+    osc.start();
 
-  window.addEventListener("keydown", incFreqDiff);
-  window.addEventListener("keyup", decFreqDiff);
+    window.addEventListener("keydown", incFreqDiff);
+    window.addEventListener("keyup", decFreqDiff);
 
-  intervalId = setInterval(function(){
-    var currFreq = osc.frequency.value;
+    intervalId = setInterval(function(){
+      var currFreq = osc.frequency.value;
 
-    if ((freqDiff < 0 && currFreq > minFreq) || (freqDiff > 0 && currFreq < maxFreq)) {
-      osc.frequency.value += freqDiff;
-    }
-  }, 40);
-});
+      if ((freqDiff < 0 && currFreq > minFreq) ||
+          (freqDiff > 0 && currFreq < maxFreq)) {
+        osc.frequency.value += freqDiff;
+      }
+    }, 40);
+  });
 
-stop.addEventListener("click", function(e) {
-  window.removeEventListener("keydown", incFreqDiff);
-  window.removeEventListener("keyup", decFreqDiff);
-  clearInterval(intervalId);
-});
+  stop.addEventListener("click", function(e) {
+    osc.stop();
+    window.removeEventListener("keydown", incFreqDiff);
+    window.removeEventListener("keyup", decFreqDiff);
+    clearInterval(intervalId);
+  });
 };
